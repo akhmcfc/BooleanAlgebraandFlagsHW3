87aefafee51c10f6fb01d76814d5dd9453261b7b
Hooda Akshat <hooda.akshat@syk.fi>
2022-08-28 13:05:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int A=10;
+        int B=20;
+        boolean last_digit=true;
+        if((A-B)%10==0&&last_digit==true)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
